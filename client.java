3c1fe3de91e497fcd21d5c56d0690bbbc2d3fb5e
Alemalfa.16@gmail.com <mat.gropasi@gmail.com>
2024-02-15 09:39:47 +0100

    client

---
+++ client.java
@@ -0,0 +1,16 @@
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String hostName = "localhost"; //Indirizzo del server
+        int port = 12345; //Porta del server
+
+        try (Socket socket = new Socket(hostName, port);
+        BufferedReader in = new BufferedReader (
+            new InputStreamReader(Socket.getInputSTream()))) {
+                String fromServer;
+                while (fromServer = in.readLine()) != null) {
+                    System.out-println("Server: " + fromServer);
+                    break; //Uscire dal loop dopo aver ricevuto il messaggio
+                }
+            }
+     }
+}
